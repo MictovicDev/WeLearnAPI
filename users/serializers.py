734bf70df8b022d30206b0d57166172677779703
MictovicDev <awaemekamichael@gmail.com>
2026-08-03 13:10:23 +0100

    fixed issues with use

--- users/serializers.py
+++ users/serializers.py
@@ -26,6 +26,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
