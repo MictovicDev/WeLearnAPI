0ebd1b3b6721453bfd540c2dcd5587146f7c6846
MictovicDev <awaemekamichael@gmail.com>
2026-08-07 20:14:53 +0100

    added changes

--- tutor_platform/settings.py
+++ tutor_platform/settings.py
@@ -257,8 +257,10 @@ CELERY_TIMEZONE = 'Africa/Lagos'
 # Email (Namecheap Private Email)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.privateemail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'support@welearnglobal.online')  # e.g. bookings@yourdomain.com
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'AF32RiP5WDG)HNc')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
