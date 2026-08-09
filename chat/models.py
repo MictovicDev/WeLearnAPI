5acadd0c3b6b1d6ed9c316948c085fd94e3894c8
MictovicDev <awaemekamichael@gmail.com>
2026-08-09 17:22:02 +0100

    added changes

--- chat/models.py
+++ chat/models.py
@@ -31,8 +31,8 @@ class ChatThread(models.Model):
 
 class Message(models.Model):
     thread = models.ForeignKey(ChatThread, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages', blank=True, null=True)
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages', blank=True, null=True)
     content = models.TextField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
