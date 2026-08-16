2673be016bbf515b2acd3796b08fc871e9124661
MictovicDev <awaemekamichael@gmail.com>
2026-08-16 21:03:52 +0100

    added changes

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Wallet, WalletTransaction
 
 # Register your models here.
+
+
+admin.site.register(WalletTransaction)
+admin.site.register(Wallet)
