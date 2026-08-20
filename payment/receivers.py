a89a3119c15f4128e712abcc29864226215ca6ca
MictovicDev <awaemekamichael@gmail.com>
2026-08-20 08:28:42 +0100

    changes

--- payment/receivers.py
+++ payment/receivers.py
@@ -8,13 +8,13 @@ from services.email_service import notify_payment_success
 @receiver(payment_succeeded)
 def payment_succeeded_handler(sender, payment, booking, **kwargs):
     wallet = booking.tutor_profile.user.wallet
-
+    print(payment)
     WalletTransaction.objects.create(
         wallet=wallet,
         type="credit",
         status="completed",
         amount=payment.amount,
-        reference=payment.reference,
+        reference=f"Booking-{payment.provider_reference[0:1]}",
         description= f"Payment from{booking.id}"
     )
     notify_payment_success(booking, payment)
