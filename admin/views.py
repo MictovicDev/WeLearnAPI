9581d19e0aefe5ba521c7b32585c792b0f12c397
MictovicDev <awaemekamichael@gmail.com>
2026-08-24 15:54:47 +0100

    changes

--- admin/views.py
+++ admin/views.py
@@ -138,7 +138,7 @@ class AdminViewSet(viewsets.GenericViewSet):
             )
             transaction = withdrawal.transaction
             transaction.status = "completed"
-            transaction.save(updated_fields=["status"])
+            transaction.save(update_fields=["status"])
         except DjangoValidationError as e:
             return Response({'detail': e.message}, status=status.HTTP_400_BAD_REQUEST)
 
